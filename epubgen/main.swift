d499ce050bd3fc6163a459fe530b7f15f6f012c6
Felix <github@drjackyl.de>
2017-05-13 15:22:43 +0200

    Import history: Initial commit

---
+++ epubgen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  epubgen
+//
+//  Created by Felix Lieb on 10.03.17.
+//  Copyright © 2017 drjackyl.de. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
